adc0597128675631f5fa86a7ee795e94fae35693
James Bint <khakimonk@gmail.com>
2016-05-19 22:28:27 +0100

    re-tooled html and js

--- JS/Twitch_APP.js
+++ JS/Twitch_APP.js
@@ -1,22 +1,26 @@
-var twitchUser = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion"];
-/*$(document).ready(function() {
-    for (var i = 0; i < twitchUser.length; i++) {
-        $('#viewerList').append('<li class="userList">' + twitchUser[i] + '<div>' + '</div></li>');
-    }
-});*/
+/*CSS Class list
+userList = twitchUser display
+twitchList = online status box
+ID list
+viewerList = ul element
+*/
 
+var twitchUser = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion", "sheevergaming", "ogamingSC2"];
 var twitchURL = "https://api.twitch.tv/kraken/streams/";
-var stream;
 
-/*$.getJSON(twitchURL, function(data) {
-    stream = data.stream;
-    console.log(stream);
-  });*/
+function streamer(twitchID, game, status, logo) {
+    this.twitchID = twitchID;
+    this.game = game;
+    this.status = status;
+    this.logo = logo;
+}
+var $list = $('#viewerList');
+var $viewerContainer = $('#viewerContainer');
+console.log($viewerContainer.attr("class"));
+
 
 function checkOnline() {
-    for (var i = 0; i < twitchUser.length; i++) {
-        findStreamer(twitchUser[i]);
-    }
+    twitchUser.forEach(findStreamer);
 }
 
 function findStreamer(twitchUser) {
@@ -24,37 +28,31 @@ function findStreamer(twitchUser) {
     console.log(twitchURL);
     $.getJSON(twitchURL, function(data) {
         useData(data, twitchUser);
-        //console.log(stream);
     });
     twitchURL = "https://api.twitch.tv/kraken/streams/";
 }
 
 function useData(data, twitchUser) {
-    var notOnline = [];
-    var online = [];
-    var stream = data;
-    if (stream.stream == null) {
-        notOnline.push(twitchUser);
-    } else if (stream.stream !== null) {
-        online.push(twitchUser);
+    if (data.stream !== null) {
+        var twitchUser = new streamer(twitchUser, data.stream.game, "online", data.stream.channel.logo);
+    } else {
+        var twitchUser = new streamer(twitchUser, null, "offline", null);
     }
-    console.log(notOnline);
-    console.log(online);
-    setOnline(online);
-    setOffline(notOnline);
+    showResult(twitchUser);
+    /*$list.append('<div class="panel panel-primary"><div class="panel-heading" id="viewerList"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.game + '</div></div>');*/
+    console.log(twitchUser);
 }
 
-
-function setOnline(online) {
-  for(var i = 0; i < online.length; i ++) {
-    $('#viewerList').append('<li class="userList">' + online[i] + '<div class="twitchList"><p>Online</p></div></li>');
-  }
-}
-
-function setOffline(notOnline) {
-  for(var i = 0; i < notOnline.length; i ++) {
-    $('#viewerList').append('<li class="userList">' + notOnline[i] + '<div class="twitchList"><p>Offline</p></div></li>');
-  }
+function showResult(twitchUser) {
+    if(twitchUser.status == "offline" && $viewerContainer.attr("class") == "jumbotron all" || $viewerContainer.attr("class") == "jumbotron offline") {
+      $list.append('<div class="panel panel-primary"><div class="panel-heading" id="viewerList"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.status + '</div></div>');
+    }
+    else if (twitchUser.status == "online" && $viewerContainer.attr("class") == "jumbotron all" || $viewerContainer.attr("class") == "jumbotron online") {
+      $list.append('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.status + '</div></div>');
+    }
+    else if (twitchUser.status == "online" && $viewerContainer.attr("class") == "jumbotron all" || $viewerContainer.attr("class") == "jumbotron online") {
+      $list.append('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.game + '</div></div>');
+    }
 }
 
 checkOnline();
