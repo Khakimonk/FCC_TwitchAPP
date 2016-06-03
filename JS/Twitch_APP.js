c5002796bff0f1f83af7ee66941ee72c196af536
James Bint <Khakimonk@gmail.com>
2016-06-03 23:59:29 +0100

    added links

--- JS/Twitch_APP.js
+++ JS/Twitch_APP.js
@@ -5,14 +5,15 @@ ID list
 viewerList = ul element
 */
 
-var twitchUser = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion", "sheevergaming", "ogamingSC2"];
+var twitchUser = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "cretetion", "sheevergaming", "ogamingSC2", "comster404"];
 var twitchURL = "https://api.twitch.tv/kraken/streams/";
 
-function streamer(twitchID, game, status, logo) {
+function streamer(twitchID, game, status, logo, url) {
     this.twitchID = twitchID;
     this.game = game;
     this.status = status;
     this.logo = logo;
+    this.url = url;
 }
 var $list = $('#viewerList');
 var $viewerContainer = $('#viewerContainer');
@@ -26,7 +27,7 @@ function checkOnline() {
 function findStreamer(twitchUser) {
     twitchURL += twitchUser;
     console.log(twitchURL);
-    $.getJSON(twitchURL, function(data) {
+    $.getJSON(twitchURL, function(data, status) {
         useData(data, twitchUser);
     });
     twitchURL = "https://api.twitch.tv/kraken/streams/";
@@ -34,13 +35,18 @@ function findStreamer(twitchUser) {
 
 function useData(data, twitchUser) {
     if (data.stream !== null) {
-        var twitchUser = new streamer(twitchUser, data.stream.game, "online", data.stream.channel.logo);
+        var twitchUser = new streamer(twitchUser, data.stream.game, "online", data.stream.channel.logo, data.stream.channel.url);
     } else {
-        var twitchUser = new streamer(twitchUser, null, "offline", null);
+        var twitchUser = new streamer(twitchUser, null, "offline", null, null);
     }
     showResult(twitchUser);
-    /*$list.append('<div class="panel panel-primary"><div class="panel-heading" id="viewerList"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.game + '</div></div>');*/
     console.log(twitchUser);
+}
+
+function noUser(twitchUser) {
+  var $foot = $('#foot');
+  var twitchUser = new streamer(twitchUser, null, "no user" , null);
+  $foot.append("<p>" + twitchUser.twitchID + " " + twitchUser.status + "</p>");
 }
 
 function showResult(twitchUser) {
@@ -56,10 +62,9 @@ function showResult(twitchUser) {
 function showAll(twitchUser) {
     if (twitchUser.status === "offline") {
         $list.append('<div class="panel panel-primary"><div class="panel-heading" id="viewerList"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.status + '</div></div>');
-    } else if (twitchUser.game != null) {
-        $list.append('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.game + '</div></div>');
-    } else if (twitchUser.status == "online") {
-        $list.append('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.status + '</div></div>');
+    }
+    else if (twitchUser.status === "online") {
+        $list.append('<div class="panel panel-primary"><div class="panel-heading"><a href="' + twitchUser.url + '"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></a></div><div class="panel-body">' + twitchUser.status + '</div></div>');
     }
 }
 
@@ -71,9 +76,9 @@ function showOffline(twitchUser) {
 
 function showOnline(twitchUser) {
   if (twitchUser.game != null) {
-      $list.append('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.game + '</div></div>');
+      $list.append('<div class="panel panel-primary"><div class="panel-heading"><a href="' + twitchUser.url + '"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></a></div><div class="panel-body">' + twitchUser.game + '</div></div>');
   } else if (twitchUser.status == "online") {
-      $list.append('<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></div><div class="panel-body">' + twitchUser.status + '</div></div>');
+      $list.append('<div class="panel panel-primary"><div class="panel-heading"><a href="' + twitchUser.url + '"><h3 class="panel-title">' + twitchUser.twitchID + '</h3></a></div><div class="panel-body">' + twitchUser.status + '</div></div>');
   }
 
 }
